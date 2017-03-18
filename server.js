c20c09799512a185cb4563d47e5757cc33386113
jgeorge97 <jgeorge97@gmail.com>
2017-03-18 19:21:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/pg1',function (req,res){
 });
 
 app.get('/pg2',function (req,res){
-    res.send("Page 2 will be loaded here!!");
+    res.send("Page 2 will be loaded here!!");});
     
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
