6857584ea58d1301d245e79d1b4120be2247854b
jgeorge97 <jgeorge97@gmail.com>
2017-03-19 17:37:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var element=document.getElementById('main');
 element.innerHTML='New Stuff';
 
 var img = document.getElementById('madi');
-madi.onclick=function(){
+img.onclick=function(){
     img.style.marginLeft = '100px';
 };
