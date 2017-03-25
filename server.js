a91c64d485dcc5a3bb094ea2d5a71f8b9c43f836
jgeorge97 <jgeorge97@gmail.com>
2017-03-25 10:36:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var counter =0;
+app.get('/counter',function(req,res){
+    counter =counter +1;
+    res.send(counter.toString());
+});
+
 var pgs={
         pg1: {
         title:'Page 1 | George Jose',
